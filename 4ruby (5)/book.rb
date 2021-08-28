5e9279b9f787b0b7b4380d23761ad6f3642063fa
yasufinc <yasuhiro.matsumoto@finc.com>
2021-08-29 02:24:01 +0900

    editted

--- 4ruby (5)/book.rb
+++ 4ruby (5)/book.rb
@@ -10,21 +10,22 @@ class Book
     @title = title
   end
 
-  def price_increase()
+  def price_increase
     @price *= 2
   end
 
 end
 
 class Comic < Book
-  def price_increase()
+  def price_increase
     @price *= 1.5
+    @price = @price.to_i
   end
 end
 
 
 class Magazine < Book
-  def price_increase()
+  def price_increase
     @price *= 4
   end
 end
@@ -38,18 +39,21 @@ book3 = Magazine.new("book3", 3000)
 
 #確認
 p book1, book2, book3
+#<Book:0x00007fb195050390 @title="book1", @price=1000>
+#<Comic:0x00007fb195050340 @title="book2", @price=2000>
+#<Magazine:0x00007fb1950502f0 @title="book3", @price=3000>
 
-p book1.title
+p book1.title #"book1"
 book1.rename_title("BOOK1")
-p book1.title
+p book1.title #"BOOK1"
 
-p book1.price
+p book1.price #1000
 book1.price_increase
-p book1.price
-p book1
+p book1.price #2000
+p book1 #<Book:0x00007fb195050390 @title="BOOK1", @price=2000>
 
-p book2.title, book3.title
-p book2.price, book3.price
+p book2.title, book3.title #"book2", "book3"
+p book2.price, book3.price #2000, 3000
 book2.price_increase
 book3.price_increase
-p book2.price, book3.price
+p book2.price, book3.price #3000, 12000
--- 4ruby (5)/kadai.rb
+++ 4ruby (5)/kadai.rb
@@ -44,7 +44,7 @@ a=1
 b=2
 c=3
 array1 = [a,b,c]
-print array1
+print array1 #[1, 2, 3]
 puts ""
 array1.each do |e|
   puts e
@@ -55,28 +55,28 @@ new = 4
 if !(array1.include?(new))
   array1 << new
 end
-print array1
+print array1 #[1, 2, 3, 4]
 puts ""
 
 #9
-def arraySum(array)
+def array_sum(array)
   sum = 0
   array.each do |e|
     sum += e
   end
   return sum
 end
-p arraySum(array1)
+p array_sum(array1) #10
 
 #10
-def arrayAve(array)
+def array_ave(array)
   sum = 0
   array.each do |e|
     sum += e
   end
   return sum.to_f/array.size
 end
-p arrayAve(array1)
+p array_ave(array1) #2.5
 
 #11
 array2 = [1,2,3,4,5,6,7,8,9]
@@ -85,7 +85,7 @@ count = 0
 array2.each do |num|
   sum += num
   if sum > 15
-    p array2[0..count]
+    p array2[0..count] #[1, 2, 3, 4, 5, 6]
     break
   end
   count += 1
@@ -93,19 +93,22 @@ end
 
 #12,13,14,15,16
 scores = {:key1 => 1, :key2 => 2, :key3 => 3}
-puts scores[:key2]
+puts scores[:key2] #2
+
 scores[:key4] = 4
-puts scores
+puts scores #{:key1=>1, :key2=>2, :key3=>3, :key4=>4}
+
 scores.each do |key, value|
   p "#{key} is #{value}"
 end
+
 scores.each do |key, value|
   scores[key] *= 2
 end
-p scores
+p scores #{:key1=>2, :key2=>4, :key3=>6, :key4=>8}
 
 #17
-(1..30).each do |i|
+(1..20).each do |i|
   if i % 15 == 0
     puts "FizzBuzz"
   elsif i % 3 == 0
@@ -120,45 +123,45 @@ end
 #18
 a1 = [1,2,3]
 a2 = a1.map{|n| n*2}
-p a2
+p a2 #[2, 4, 6]
 
 #19
-def checkArray(array,target)
+def check_array(array,target)
   if array.include?(target)
-    return TRUE
+    return true
   else
-    return FALSE
+    return false
   end
 end
-p checkArray(a1, 5)
+p check_array(a1, 5) #false
 
 #20
-def minArray(array)
+def min_array(array)
   min = Float::INFINITY
-  array.size.times do |i|
+  (0..array.size-1).each do |i|
     if array[i] < min
       min = array[i]
     end
   end
   return min
 end
-p minArray(a1)
+p min_array(a1) #1
 
 #21
-def dupcheckArray(array)
+def dup_check_array(array)
   (0..array.size-1).each do |i|
     (i+1..array.size-1).each do |j|
       if array[i] == array[j]
-        return TRUE
+        return true
       end
     end
   end
-  return FALSE
+  return false
 end
-p dupcheckArray(a1)
+p dup_check_array(a1) #false
 
 #22
-def dupArray(array)
+def dup_array(array)
   d = [ ]
   (0..array.size-1).each do |i|
     (i+1..array.size-1).each do |j|
@@ -171,11 +174,11 @@ def dupArray(array)
   return d.uniq.sort
 end
 a3 = [1,3,2,3,8,3,2]
-p dupArray(a3)
+p dup_array(a3) #[2, 3]
 
 #23
-def dupHash(array)
-  d = dupArray(array)
+def dup_hash(array)
+  d = dup_array(array)
   ans = {}
   d.each do |num|
     ans[num] = array.count(num)
@@ -183,4 +186,4 @@ def dupHash(array)
   return ans
 end
 a3 = [1,3,2,3,8,3,2]
-p dupHash(a3)
+p dup_hash(a3) #{2=>2, 3=>3}
